2911b10942255db92392a72c63c61a8cc4d63293
Joe Mercer <mynameisjoemercer@gmail.com>
2014-04-20 11:08:32 -0400

    fixed spacing

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,206 +27,206 @@ module.exports = function(grunt) {
       prod: ['dist']
     },
 
-      browserify: {
-          vendor: {
-              src: ['client/requires/**/*.js'],
-              dest: 'build/vendor.js',
-              options: {
-                  shim: {
-                      jquery: {
-                          path: 'client/requires/jquery/js/jquery.js',
-                          exports: '$'
-                      },
-                      underscore: {
-                          path: 'client/requires/underscore/js/underscore.js',
-                          exports: '_'
-                      },
-                      backbone: {
-                          path: 'client/requires/backbone/js/backbone.js',
-                          exports: 'Backbone',
-                          depends: {
-                              underscore: 'underscore'
-                          }
-                      },
-                      'backbone.marionette': {
-                          path: 'client/requires/backbone.marionette/js/backbone.marionette.js',
-                          exports: 'Marionette',
-                          depends: {
-                              jquery: '$',
-                              backbone: 'Backbone',
-                              underscore: '_'
-                          }
-                      }
-                  }
-              }
-          },
-          app: {
-              files: {
-                  'build/app.js': ['client/src/main.js']
-              },
-              options: {
-                  transform: ['hbsfy'],
-                  external: ['jquery', 'underscore', 'backbone', 'backbone.marionette']
-              }
-          },
-          test: {
-              files: {
-                  'build/tests.js': [
-                      'client/spec/**/*.test.js'
-                  ]
-              },
-              options: {
-                  transform: ['hbsfy'],
-                  external: ['jquery', 'underscore', 'backbone', 'backbone.marionette']
-              }
-          }
-      },
-
-      less: {
-          transpile: {
-              files: {
-                  'build/<%= pkg.name %>.css': [
-                      'client/styles/reset.css',
-                      'client/requires/*/css/*',
-                      'client/styles/less/main.less'
-                  ]
-              }
-          }
-      },
-
-      concat: {
-          'build/<%= pkg.name %>.js': ['build/vendor.js', 'build/app.js']
-      },
-
-      copy: {
-          dev: {
-              files: [{
-                  src: 'build/<%= pkg.name %>.js',
-                  dest: 'public/js/<%= pkg.name %>.js'
-              }, {
-                  src: 'build/<%= pkg.name %>.css',
-                  dest: 'public/css/<%= pkg.name %>.css'
-              }, {
-                  src: 'client/img/*',
-                  dest: 'public/img/'
-              }]
-          },
-          prod: {
-              files: [{
-                  src: ['client/img/*'],
-                  dest: 'dist/img/'
-              }]
-          }
-      },
-
-      // CSS minification.
-      cssmin: {
-          minify: {
-              src: ['build/<%= pkg.name %>.css'],
-              dest: 'dist/css/<%= pkg.name %>.css'
-          }
-      },
-
-      // Javascript minification.
-      uglify: {
-          compile: {
-              options: {
-                  compress: true,
-                  verbose: true
-              },
-              files: [{
-                  src: 'build/<%= pkg.name %>.js',
-                  dest: 'dist/js/<%= pkg.name %>.js'
-              }]
-          }
-      },
-
-      // for changes to the front-end code
-      watch: {
-          scripts: {
-              files: ['client/templates/*.hbs', 'client/src/**/*.js'],
-              tasks: ['clean:dev', 'browserify:app', 'concat', 'copy:dev']
-          },
-          less: {
-              files: ['client/styles/**/*.less'],
-              tasks: ['less:transpile', 'copy:dev']
-          },
-          test: {
-              files: ['build/app.js', 'client/spec/**/*.test.js'],
-              tasks: ['browserify:test']
-          },
-          karma: {
-              files: ['build/tests.js'],
-              tasks: ['jshint:test', 'karma:watcher:run']
-          }
-      },
-
-      nodemon: {
-        dev: {
+    browserify: {
+      vendor: {
+          src: ['client/requires/**/*.js'],
+          dest: 'build/vendor.js',
           options: {
-            file: 'server.js',
-            nodeArgs: ['--debug'],
-            watchedFolders: [],
-            env: {
-              PORT: '3300'
+            shim: {
+              jquery: {
+                path: 'client/requires/jquery/js/jquery.js',
+                exports: '$'
+              },
+              underscore: {
+                path: 'client/requires/underscore/js/underscore.js',
+                exports: '_'
+              },
+              backbone: {
+                path: 'client/requires/backbone/js/backbone.js',
+                exports: 'Backbone',
+                depends: {
+                  underscore: 'underscore'
+                }
+              },
+              'backbone.marionette': {
+                path: 'client/requires/backbone.marionette/js/backbone.marionette.js',
+                exports: 'Marionette',
+                depends: {
+                  jquery: '$',
+                  backbone: 'Backbone',
+                  underscore: '_'
+                }
+              }
             }
           }
-        }
-      },
-
-      // server tests
-      simplemocha: {
-        options: {
-          globals: ['expect', 'sinon'],
-          timeout: 3000,
-          ignoreLeaks: false,
-          ui: 'bdd',
-          reporter: 'spec'
         },
-
-        server: {
-          src: ['spec/spechelper.js', 'spec/**/*.test.js']
-        }
-      },
-
-      shell: {
-
-      },
-
-      concurrent: {
-          dev: {
-              tasks: ['nodemon:dev', 'watch:scripts', 'watch:less', 'watch:test'],
-              options: {
-                  logConcurrentOutput: true
-              }
+        app: {
+          files: {
+            'build/app.js': ['client/src/main.js']
           },
-          test: {
-              tasks: ['watch:karma'],
-              options: {
-                  logConcurrentOutput: true
-              }
+          options: {
+            transform: ['hbsfy'],
+            external: ['jquery', 'underscore', 'backbone', 'backbone.marionette']
           }
-      },
-
-      // for front-end tdd
-      karma: {
-        options: {
-          configFile: 'karma.conf.js'
-        },
-        watcher: {
-          background: true,
-          singleRun: false
         },
         test: {
-          singleRun: true
+          files: {
+            'build/tests.js': [
+              'client/spec/**/*.test.js'
+            ]
+          },
+          options: {
+            transform: ['hbsfy'],
+            external: ['jquery', 'underscore', 'backbone', 'backbone.marionette']
+          }
         }
+    },
+
+    less: {
+      transpile: {
+        files: {
+          'build/<%= pkg.name %>.css': [
+            'client/styles/reset.css',
+            'client/requires/*/css/*',
+            'client/styles/less/main.less'
+          ]
+        }
+      }
+    },
+
+    concat: {
+      'build/<%= pkg.name %>.js': ['build/vendor.js', 'build/app.js']
+    },
+
+    copy: {
+      dev: {
+        files: [{
+          src: 'build/<%= pkg.name %>.js',
+          dest: 'public/js/<%= pkg.name %>.js'
+        }, {
+          src: 'build/<%= pkg.name %>.css',
+          dest: 'public/css/<%= pkg.name %>.css'
+        }, {
+          src: 'client/img/*',
+          dest: 'public/img/'
+        }]
+      },
+      prod: {
+        files: [{
+          src: ['client/img/*'],
+          dest: 'dist/img/'
+        }]
+      }
+    },
+
+    // CSS minification.
+    cssmin: {
+      minify: {
+        src: ['build/<%= pkg.name %>.css'],
+        dest: 'dist/css/<%= pkg.name %>.css'
+      }
+    },
+
+    // Javascript minification.
+    uglify: {
+      compile: {
+        options: {
+          compress: true,
+          verbose: true
+        },
+        files: [{
+          src: 'build/<%= pkg.name %>.js',
+          dest: 'dist/js/<%= pkg.name %>.js'
+        }]
+      }
+    },
+
+    // for changes to the front-end code
+    watch: {
+      scripts: {
+        files: ['client/templates/*.hbs', 'client/src/**/*.js'],
+        tasks: ['clean:dev', 'browserify:app', 'concat', 'copy:dev']
+      },
+      less: {
+        files: ['client/styles/**/*.less'],
+        tasks: ['less:transpile', 'copy:dev']
+      },
+      test: {
+        files: ['build/app.js', 'client/spec/**/*.test.js'],
+        tasks: ['browserify:test']
+      },
+      karma: {
+        files: ['build/tests.js'],
+        tasks: ['jshint:test', 'karma:watcher:run']
+      }
+    },
+
+    nodemon: {
+      dev: {
+        options: {
+          file: 'server.js',
+          nodeArgs: ['--debug'],
+          watchedFolders: [],
+          env: {
+            PORT: '3300'
+          }
+        }
+      }
+    },
+
+    // server tests
+    simplemocha: {
+      options: {
+        globals: ['expect', 'sinon'],
+        timeout: 3000,
+        ignoreLeaks: false,
+        ui: 'bdd',
+        reporter: 'spec'
       },
 
-      jshint: {
-          all: ['Gruntfile.js', 'client/src/**/*.js', 'client/spec/**/*.js'],
-          dev: ['client/src/**/*.js'],
-          test: ['client/spec/**/*.js']
+      server: {
+        src: ['spec/spechelper.js', 'spec/**/*.test.js']
       }
+    },
+
+    shell: {
+
+    },
+
+    concurrent: {
+      dev: {
+        tasks: ['nodemon:dev', 'watch:scripts', 'watch:less', 'watch:test'],
+        options: {
+          logConcurrentOutput: true
+        }
+      },
+      test: {
+        tasks: ['watch:karma'],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
+    },
+
+    // for front-end tdd
+    karma: {
+      options: {
+        configFile: 'karma.conf.js'
+      },
+      watcher: {
+        background: true,
+        singleRun: false
+      },
+      test: {
+        singleRun: true
+      }
+    },
+
+    jshint: {
+      all: ['Gruntfile.js', 'client/src/**/*.js', 'client/spec/**/*.js'],
+      dev: ['client/src/**/*.js'],
+      test: ['client/spec/**/*.js']
+    }
   });
 
   grunt.registerTask('init:dev', ['clean', 'bower', 'browserify:vendor']);
