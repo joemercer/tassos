8fc44350aac310848b2070eccf1fd02edd6d9184
Joe Mercer <mynameisjoemercer@gmail.com>
2014-05-08 22:30:24 -0400

    navbar work

--- client/src/main.js
+++ client/src/main.js
@@ -5,7 +5,28 @@ app.start();
 var $ = require('jquery');
 
 $(function(){
+	"use strict";
 	$('img').click(function(){
 		window.App.router.navigate('blog', {trigger: true});
+	});
+
+	var $window = $(window);
+
+	// # Nav
+	// - fix the nav
+
+	var $nav = $('.nav-container');
+	var fixNavTrigger = $nav.offset().top;
+	$window.scroll(function(){
+    if ($window.scrollTop() >= fixNavTrigger){
+			if (!$nav.hasClass('fixed')) {
+				$nav.addClass('fixed');
+			}
+    }
+    else{
+			if ($nav.hasClass('fixed')) {
+				$nav.removeClass('fixed');
+			}
+    }
 	});
 });
