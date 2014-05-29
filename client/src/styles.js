6bfd199b25805c5557abbf0c2117c9a07d5f6dfd
Joe Mercer <mynameisjoemercer@gmail.com>
2014-05-29 00:35:04 -0400

    bundle nav fixing stuff together into a Nav object

--- client/src/styles.js
+++ client/src/styles.js
@@ -9,47 +9,47 @@ $(function(){
 	// - fixes the nav to the top of the screen
 	// _______
 
-	var $nav = $('.nav-container');
-	var $navBuffer = $('.nav-fixed-buffer')
-
-
-
-	var getFixNavTrigger = function() {
-		if ($nav.hasClass('fixed')) {
-			return $navBuffer.offset().top - parseInt($nav.css('margin-top'));
-		}
-		else {
-			return $nav.offset().top - parseInt($nav.css('margin-top'));
-		}
-	}
-
-	var fixNavTrigger = getFixNavTrigger();
-
-	var fixNav = function() {
-		if ($window.scrollTop() >= fixNavTrigger) {
-			if (!$nav.hasClass('fixed')) {
-				$nav.addClass('fixed');
-				$navBuffer.removeClass('hide');
-				$nav.find('.nav-inner-container').addClass('container');
+	var Nav = {
+		$window: $window,
+		$nav: $('.nav-container'),
+		$navBuffer: $('.nav-fixed-buffer'),
+		setTrigger: function() {
+			this.navMarginTop = parseInt(this.$nav.css('margin-top'));
+			if (this.$nav.hasClass('fixed')) {
+				this.trigger = this.$navBuffer.offset().top - this.navMarginTop;
 			}
-    }
-    else {
-			if ($nav.hasClass('fixed')) {
-				$nav.removeClass('fixed');
-				$navBuffer.addClass('hide');
-				$nav.find('.nav-inner-container').removeClass('container');
+			else {
+				this.trigger = this.$nav.offset().top - this.navMarginTop;
 			}
-    }
-	}
+		},
+		maybeFixNav: function() {
+			if (this.$window.scrollTop() >= this.trigger) {
+				if (!this.$nav.hasClass('fixed')) {
+					this.$nav.addClass('fixed');
+					this.$navBuffer.removeClass('hide');
+					this.$nav.find('.nav-inner-container').addClass('container');
+				}
+	    }
+	    else {
+				if (this.$nav.hasClass('fixed')) {
+					this.$nav.removeClass('fixed');
+					this.$navBuffer.addClass('hide');
+					this.$nav.find('.nav-inner-container').removeClass('container');
+				}
+	    }
+		}
+	};
+	Nav.setTrigger();
+	Nav.maybeFixNav();
 
 	// recalculate the trigger when window is resized
 	$window.resize(function(e){
-		fixNavTrigger = getFixNavTrigger();
-		fixNav();
+		Nav.setTrigger();
+		Nav.maybeFixNav();
 	});
 
 	$window.scroll(function(){
-		fixNav();
+		Nav.maybeFixNav();
 	});
 
 	// # Toggle the nav
