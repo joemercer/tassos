23003d5385e7eceee8b64f3915de6282fe4d4005
Joe Mercer <mynameisjoemercer@gmail.com>
2014-06-18 00:21:22 -0400

    added angular as a dependency

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,26 @@
 var $ = require('jquery');
 var _ = require('lodash');
 
+var angular = require('angular');
+
+var App = angular.module('Tassos', []);
+
+App.controller('testController', function ($scope) {
+  $scope.things = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+});
+
 $(function(){
 	'use strict';
 
 	var $window = $(window);
+
+	console.log('jquery yo');
 
 });
--- shims.js
+++ shims.js
@@ -1,4 +1,5 @@
 module.exports = {
+	angular: {exports: "angular"},
   jquery: {exports: "jQuery"},
   lodash: {exports: "_"}
 };
