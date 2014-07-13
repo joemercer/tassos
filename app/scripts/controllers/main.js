8cce1a6ed1e678b5710cabda73a02009ae62a830
Joe Mercer <mynameisjoemercer@gmail.com>
2014-07-13 15:06:17 -0400

    spinner

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,3 +1,4 @@
+/*global $:false */
 'use strict';
 
 /**
@@ -15,8 +16,21 @@ angular.module('tassosApp')
     	description: 'You gotta if you wanna start the game o_O'
     };
 
-    $scope.getCard = function() {
+    $scope.next = function() {
     	$scope.card = CardsService.get();
+
+    	if ($scope.card.secondspin > 0) {
+    		$scope.spin();
+    	}
+    };
+
+    $scope.degrees = 0;
+    $scope.spin = function() {
+    	$scope.degrees += 360 + (720 * Math.random());
+    	$('.spinner').css({'-webkit-transform' : 'rotate('+ $scope.degrees +'deg)',
+											 	 '-moz-transform' : 'rotate('+ $scope.degrees +'deg)',
+												 '-ms-transform' : 'rotate('+ $scope.degrees +'deg)',
+												 'transform' : 'rotate('+ $scope.degrees +'deg)'});
     };
 
   });
